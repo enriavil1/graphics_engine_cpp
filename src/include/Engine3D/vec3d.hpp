acf82b4eafd067ce7496088ec0bb1ecebde92bcb
Enrique Jose Avila Asapche <eavilaasapche@gmail.com>
2025-02-10 14:01:53 +0300

    added vector operations

--- src/include/Engine3D/vec3d.hpp
+++ src/include/Engine3D/vec3d.hpp
@@ -9,20 +9,29 @@ public:
   double x = 0;
   double y = 0;
   double z = 0;
+  double w = 1;
 
   Vec3D() {};
   Vec3D(double x, double y, double z) : x(x), y(y), z(z) {};
 
-  ImVec4 getImVec4() {
-    return ImVec4(this->x, this->y, this->z,
-                  0 /* We dont need the last coordinate */);
-  }
+  ImVec4 getImVec4() { return ImVec4(this->x, this->y, this->z, this->w); }
 
   ImVec2 getImVec2(const ImVec2 &window_pos) {
     return ImVec2(this->x + window_pos.x, this->y + window_pos.y);
   }
 
   ImVec2 getImVec2() { return ImVec2(this->x, this->y); };
+
+  Vec3D operator+(const Vec3D &vec3d);
+  Vec3D operator-(const Vec3D &vec3d);
+  Vec3D operator*(const Vec3D &vec3d);
+  Vec3D operator/(const Vec3D &vec3d);
+
+  float getDotProduct(const Vec3D &vec3d);
+  float getVectorLength(const Vec3D &vec3d);
+
+  Vec3D getCrossProduct(const Vec3D &vec3d);
+  Vec3D normalize(const Vec3D &vec3d);
 };
 
 } // namespace engine3D
