d6c380d3fc1106236191ec88493fc822605cfed3
Enrique Jose Avila Asapche <eavilaasapche@gmail.com>
2025-04-30 21:41:32 +0100

    added clipping methods

--- src/include/Engine3D/Engine3D.hpp
+++ src/include/Engine3D/Engine3D.hpp
@@ -8,6 +8,9 @@
 
 #include <string>
 
+#define NEAR 0.1
+#define FAR 1.0
+
 namespace engine3D {
 
 class Engine {
@@ -25,10 +28,18 @@ private:
 
   inline static std::shared_ptr<Object3D> mp_projecting_obj = nullptr;
 
-  inline static Camera mp_camera = Camera();
+  inline static Camera mp_camera = Camera(NEAR, FAR);
 
   static void multiplyVectorMatrix(const Vec3D &point, Vec3D &output,
                                    const Matrix4x4 &m);
+
+  static Vec3D getPlaneInterception(const Vec3D &point_on_plane,
+                                    const Vec3D &plane,
+                                    const Vec3D &start_of_line,
+                                    const Vec3D &end_of_line);
+  static int clipTriangle(const Vec3D &plane_a, const Vec3D &plane_b,
+                          Triangle &triangle,
+                          std::vector<Triangle> &triangles_output);
 
   static void scaleTriangle(Triangle &triangle);
   static void scaleVec2d(ImVec2 &point);
--- src/lib/Engine3D/Engine3D.cpp
+++ src/lib/Engine3D/Engine3D.cpp
@@ -39,12 +39,14 @@ void Engine::project(double theta) {
   auto world_matrix = Matrix4x4::getWorldMatrix() *
                       Matrix4x4::getTranslationMatrix(0.0f, 0.0f, 5.0f);
 
-  const auto &projection_matrix = Matrix4x4::getProjectionMatrix(ASPECT_RATIO);
+  const auto &projection_matrix = Matrix4x4::getProjectionMatrix(
+      Engine::getCamera().getNear(), ASPECT_RATIO);
   const auto &view_matrix = Engine::getCamera().getLookAtMatrix();
 
-  std::vector<Triangle> triangles_to_draw;
-
+  // as we load the obj we have the vertecis inverted
   const auto &xy_inversion_vector = Vec3D(-1.0f, -1.0f, 0);
+
+  std::vector<Triangle> triangles_to_draw;
 
   for (auto &tri : Engine::mp_projecting_obj->getMesh().triangles) {
     Triangle projected_triangle;
@@ -60,6 +62,8 @@ void Engine::project(double theta) {
       for (int i = 0; i < projected_triangle.points.size(); ++i) {
         projected_triangle.points[i] =
             (projected_triangle.points[i] * view_matrix) * projection_matrix;
+
+        // clip against for z value
 
         projected_triangle.points[i] =
             (projected_triangle.points[i] / projected_triangle.points[i].w) *
@@ -104,6 +108,122 @@ void Engine::project(double theta) {
       draw_list->AddLine(drawing_points[1], drawing_points[2], IM_COL32_BLACK);
     }
   }
+};
+
+Vec3D Engine::getPlaneInterception(
+    const Vec3D &point_on_plane, const Vec3D &plane, const Vec3D &start_of_line,
+    const Vec3D &end_of_line) { // for safety normalize the plane
+  const auto &plane_normal = plane.normalize();
+  const auto &point_on_plane_normal =
+      plane_normal.getDotProduct(point_on_plane);
+
+  // getting plane interception is just:
+  // plane_x * point_x + plane_y * point_y + plane_z * point_z
+  const auto &start_of_line_interception =
+      plane_normal.getDotProduct(start_of_line);
+  const auto &end_of_line_interception =
+      plane_normal.getDotProduct(end_of_line);
+
+  /*
+   *
+   * Because a point = { x, y, z}
+   * point.x = mt + b
+   * point.y = mt + b
+   * point.z = mt + b
+   *
+   * we have to input those points into our plane normal and solve for t
+   * the line * t = the line to point of interception
+   *
+   * */
+
+  float t = (point_on_plane_normal - start_of_line_interception) /
+            (end_of_line_interception - start_of_line_interception);
+
+  const auto &line_from_start_to_end_point = end_of_line - start_of_line;
+  const auto &line_to_intersect = line_from_start_to_end_point * t;
+
+  // return point of interception
+  return line_to_intersect + start_of_line;
+}
+
+int Engine::clipTriangle(const Vec3D &point_on_plane, const Vec3D &plane,
+                         Triangle &triangle,
+                         std::vector<Triangle> &triangles_output) {
+
+  const auto &plane_normal = plane.normalize();
+  std::array<Vec3D, 2> inside_points;
+  int inside_point_idx = 0;
+
+  std::array<Vec3D, 2> outside_points;
+  int outside_point_idx = 0;
+
+  for (const auto &point : triangle.points) {
+
+    const auto &dst_from_point_to_plane =
+        plane_normal.getDotProduct(point) -
+        plane_normal.getDotProduct(point_on_plane);
+
+    if (dst_from_point_to_plane >= 0) {
+      // if all points are inside we just return the current triangle
+      if (inside_point_idx == 1) {
+        triangles_output.push_back(triangle);
+        return 1;
+      }
+
+      inside_points[inside_point_idx] = point;
+      ++inside_point_idx;
+    } else {
+      // if all points are outside we just return
+      if (outside_point_idx == 2) {
+        return 0;
+      }
+      outside_points[outside_point_idx] = point;
+      ++outside_point_idx;
+    }
+  }
+
+  // we have two points inside and one outside
+  if (inside_point_idx == 1) {
+    Triangle new_triangle_1, new_triangle_2;
+
+    // we fill in the valid points
+    new_triangle_1.points[0] = inside_points[0];
+    new_triangle_2.points[0] = inside_points[0];
+
+    new_triangle_1.points[1] = inside_points[1];
+
+    const auto &new_point_1 = Engine::getPlaneInterception(
+        point_on_plane, plane_normal, inside_points[0], outside_points[0]);
+
+    new_triangle_1.points[2] = new_point_1;
+    new_triangle_2.points[1] = new_point_1;
+
+    new_triangle_2.points[2] = Engine::getPlaneInterception(
+        point_on_plane, plane_normal, inside_points[1], outside_points[0]);
+
+    triangles_output.push_back(new_triangle_1);
+    triangles_output.push_back(new_triangle_2);
+    return 2;
+  }
+
+  // we have two points outside the plane
+  if (inside_point_idx == 0) {
+    Triangle new_triangle;
+
+    // we fill in the valid points
+    new_triangle.points[0] = inside_points[0];
+
+    // we get the interception to the plane and set them as points
+    new_triangle.points[1] = Engine::getPlaneInterception(
+        point_on_plane, plane_normal, inside_points[0], outside_points[0]);
+    new_triangle.points[2] = Engine::getPlaneInterception(
+        point_on_plane, plane_normal, inside_points[0], outside_points[1]);
+
+    triangles_output.push_back(new_triangle);
+    return 1;
+  }
+
+  return 0;
 };
 
 void Engine::scaleTriangle(Triangle &triangle) {
